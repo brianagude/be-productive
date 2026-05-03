b96f0fb74c99619773d00ecb7eca337ddc4c33ef
Briana Gude <briana.gude@gmail.com>
2026-05-03 19:40:44 -0400

    fix build error

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -2,6 +2,8 @@ import { createClient } from '@/lib/supabase/server'
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest, NextResponse } from 'next/server'
 
+export const runtime = 'edge'
+
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
